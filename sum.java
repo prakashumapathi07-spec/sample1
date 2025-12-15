3c8ec0b178ccde23d113cdd46552e7d180791b13
prakashumapathi07-spec <prakashumapathi07@gmail.com>
2025-12-15 12:27:41 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import java.util.*;
+public class sum {
+ public static void main(String[] args) {
+ Scanner sc = new Scanner(System.in);
+ System.out.println("Enter the number:");
+ int num = sc.nextInt();
+ int sum = 0;
+ for (int i = 0; i <= num; i += 2) {
+ sum += (i * i);
+ }
+ System.out.println(sum);
+ }
+}
